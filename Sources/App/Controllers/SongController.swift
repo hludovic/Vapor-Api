94779a64c28e0e925b16bb562237bc744470c7b7
Ludovic HENRY <badsteam@hotmail.com>
2023-07-22 23:44:45 -0400

    Improvement on the controllers

--- Sources/App/Controllers/SongController.swift
+++ Sources/App/Controllers/SongController.swift
@@ -15,39 +15,41 @@ struct SongController: RouteCollection {
         songs.post(use: create)
         songs.put(use: update)
         songs.delete(":songID", use: delete)
-//        songs.group(":songID") { song in
-//            song.delete(use: delete)
-//        }
     }
 
     // GET request /songs route
     func index(req: Request) async throws -> [Song] {
-        return try await Song.query(on: req.db).all()
+        try await Song.query(on: req.db).all()
     }
 
     // POST request /songs route
-    func create(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+    func create(req: Request) async throws -> HTTPStatus {
         let song = try req.content.decode(Song.self)
-        return song.save(on: req.db).transform(to: .ok)
+        try await song.save(on: req.db)
+        return .ok
     }
 
     // PUT request /songs route
-    func update(req: Request) throws -> EventLoopFuture<HTTPStatus> {
+    func update(req: Request) async throws -> HTTPStatus {
         let song = try req.content.decode(Song.self)
-        return Song.find(song.id, on: req.db)
-            .unwrap(or: Abort(.notFound))
-            .flatMap {
-                $0.title = song.title
-                return $0.update(on: req.db).transform(to: HTTPResponseStatus.ok)
-            }
+        guard let songFromDB = try await Song.find(song.id, on: req.db) else {
+            throw Abort(.notFound)
+        }
+        songFromDB.title = song.title
+        try await songFromDB.update(on: req.db)
+        return .ok
     }
 
     // DELETE request /songs/id route
-    func delete(req: Request) throws -> EventLoopFuture<HTTPStatus> {
-        Song.find(req.parameters.get("songID"), on: req.db)
-            .unwrap(or: Abort(.notFound))
-            .flatMap { $0.delete(on: req.db) }
-            .transform(to: .ok)
+    func delete(req: Request) async throws -> HTTPStatus {
+        guard let songID = req.parameters.get("songID") else {
+            throw Abort(.notFound)
+        }
+        guard let songFromDB = try await Song.find(UUID(uuidString: songID), on: req.db) else {
+            throw Abort(.notFound)
+        }
+        try await songFromDB.delete(on: req.db)
+        return .ok
     }
 
 }
